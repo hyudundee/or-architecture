6631bc40e70ba21bfb456978e00a19477ad596b7
Haoran Yu <hyudundee@gmail.com>
2022-01-25 15:43:12 -0800

    Updates

--- src/data/menuData.js
+++
@@ -1,7 +0,0 @@
-export const menuData = [
-    { title: "首页", link: "/Home" },
-    { title: "项目", link: "/Works" },
-    { title: "简介", link: "Profile" },
-    { title: "机器时代", link: "Algorithm era" },
-    { title: "返璞归真", link: "/ Return to innocence" },
-]
